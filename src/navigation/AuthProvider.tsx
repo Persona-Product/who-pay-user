a71e383694cff636b4b9f2c5b8cd89e5a31cbc19
miyasan_0301 <miyasan.19990301@gmail.com>
2021-11-13 02:16:23 +0900

    🔥 firebase config

--- src/navigation/AuthProvider.tsx
+++ src/navigation/AuthProvider.tsx
@@ -1,20 +1,5 @@
-import type { User } from "firebase";
-import type { ReactNode } from "react";
-import React, { createContext, useState } from "react";
+import React from "react";
 
-type ContextProps = {
-	user: User | null;
-	setUser: React.Dispatch<React.SetStateAction<User | null>>;
-};
-
-type Props = {
-	children: ReactNode;
-};
-
-export const AuthUserContext = createContext({} as ContextProps);
-
-export const AuthUserProvider: React.FC<Props> = (props) => {
-	const [user, setUser] = useState<User | null>(null);
-
-	return <AuthUserContext.Provider value={{ user, setUser }}>{props.children}</AuthUserContext.Provider>;
+export const AuthProvider = () => {
+	return <div></div>;
 };
